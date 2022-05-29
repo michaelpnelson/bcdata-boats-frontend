7fe71d947992bf15379bf4d34f35aebe285451af
Michael Nelson <michael.nelson@gov.bc.ca>
2022-05-29 10:32:26 -0700

    Kanban board added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import BoatList from './BoatList.js';
+import Board, { moveCard } from '@asseinfo/react-kanban';
+import '@asseinfo/react-kanban/dist/styles.css';
+import './App.css';
+
+const startingBoard = {
+  columns: [
+    {
+      id: 1,
+      title: 'Backlog',
+      cards: [
+        {
+          id: 1,
+          title: 'Add card',
+          description: 'Add capability to add a card in a column'
+        },
+      ]
+    },
+    {
+      id: 2,
+      title: 'Doing',
+      cards: [
+        {
+          id: 2,
+          title: 'Drag-n-drop support',
+          description: 'Move a card between the columns'
+        },
+      ]
+    }
+  ]
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header"> */}
         <h1>Boat Tracker</h1>
+        <Board initialBoard={startingBoard} 
+          allowRemoveLane
+          allowRenameColumn
+          allowRemoveCard
+          onLaneRemove={console.log}
+          onCardRemove={console.log}
+          onLaneRename={console.log}
+          allowAddCard={{ on: "top" }}
+          onNewCardConfirm={draftCard => ({
+            id: new Date().getTime(),
+            ...draftCard
+          })}
+          onCardNew={console.log}/>
         <BoatList/>
         <p>
           More to come!
@@ -19,11 +62,13 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      {/* </header> */}
     </div>
   );
 }
 
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 
 export default App;
