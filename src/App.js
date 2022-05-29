6a16138dbe8d34f9d9b6ebc672a7894d5180b651
Michael Nelson <michael.nelson@gov.bc.ca>
2022-05-28 22:52:09 -0700

    Attempting API fetch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
@@ -7,6 +9,7 @@ function App() {
       <header className="App-header">
         <h1>Boat Tracker</h1>
         <img src={logo} className="App-logo" alt="logo" />
+        <BoatList/>
         <p>
           More to come!
         </p>
@@ -21,6 +24,63 @@ function App() {
       </header>
     </div>
   );
+}
+
+class BoatList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    const backendURL = process.env.BACKEND-URL;
+    // const url = 'https://reactjs.org';
+    const url = 'https://protected-plateau-53607.herokuapp.com/boats';
+    const args = {
+      mode: 'no-cors'
+    };
+
+    fetch(url, args)
+    .then(res => res.text())
+    .then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          boats: result
+        });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+  }
+
+  render() {
+    const { error, isLoaded, boats } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return <p>{boats}</p>;
+      // return (
+      //   <ul>
+      //     {boats.map(boat => (
+      //       <li>
+      //         {boat}
+      //       </li>
+      //     ))}
+      //   </ul>
+      // );
+    }
+  }
 }
 
 export default App;
